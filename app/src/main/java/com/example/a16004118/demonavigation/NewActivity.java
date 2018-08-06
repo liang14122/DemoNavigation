d1492607da20701e3499c12a439cbf5114caf0d7
liang3336 <632944695@qq.com>
2018-08-06 15:19:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16004118/demonavigation/NewActivity.java
@@ -0,0 +1,21 @@
+package com.example.a16004118.demonavigation;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
